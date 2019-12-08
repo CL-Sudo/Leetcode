1da70fd8aec9f663ff896c1e333cb4b34e26f9b8
CL-Sudo <acl0002@protonmail.ch>
2019-12-08 14:01:26 +0800

    some changes

--- Array/152_Maximum_Product_Subarray/main.cpp
+++ Array/152_Maximum_Product_Subarray/main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
+int max(int a, int b, int c)
+{
+    int max;
+
+    max = (a > b)?a:b;
+    max = (b > c)?b:c;
+
+    return max;
+
+}
+
+int maxProduct(vector<int>& nums) {
+
+    int lmax1 = 0, lmax2 = 0, gmax = INT_MIN;
+
+    for (auto a : nums) {
+
+        
+    }
+
+
+        
+    return gmax;    
+}
+
 int main()
 {   
+    vector<int> nums{2,3,-2,4};
 
+    cout << maxProduct(nums) << endl;
 
     return 0;
 }
