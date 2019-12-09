aca534676de751f4a1ad083108f80da6cf00013e
CL-Sudo <acl0002@protonmail.ch>
2019-12-09 17:15:35 +0800

    " "

--- Array/152_Maximum_Product_Subarray/main.cpp
+++ Array/152_Maximum_Product_Subarray/main.cpp
@@ -1,37 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <algorithm>
 
 using namespace std;
 
-int max(int a, int b, int c)
+int maximum(int a, int b, int c)
 {
-    int max;
+    a = (a > b) ? a : b;
+    a = (a > c) ? a : c;
 
-    max = (a > b)?a:b;
-    max = (b > c)?b:c;
+    return a;
+}
+int minimum(int a, int b, int c)
+{
+    a = (a < b) ? a : b;
+    a = (a < c) ? a : c;
 
-    return max;
-
+    return a;
 }
 
 int maxProduct(vector<int>& nums) {
 
-    int lmax1 = 0, lmax2 = 0, gmax = INT_MIN;
+    int localMax, localMin, globalMax = INT_MIN;
+    int tempMax = 1, tempMin = 1;
 
-    for (auto a : nums) {
+    for (int i = 0; i < nums.size(); i++) {
 
-        
+        localMin = minimum(nums[i], nums[i]*tempMin,nums[i]*tempMax);
+        localMax = maximum(nums[i], nums[i]*tempMin,nums[i]*tempMax);
+        globalMax = max(localMax,globalMax);
+        tempMin = localMin; tempMax = localMax;
     }
-
-
-        
-    return gmax;    
+    return globalMax;    
 }
 
 int main()
 {   
-    vector<int> nums{2,3,-2,4};
+    vector<int> nums{2,-5,-2,-4,3};
 
     cout << maxProduct(nums) << endl;
 
